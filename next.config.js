0041fe58e40389e70c0bb39eede8907ea783d34e
Rsilva305 <silva.ruben@gmail.com>
2025-05-12 19:07:37 -0400

    Remove experimental options from Next.js config

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
-  // Required for handling uploaded audio/video files
-  experimental: {
-    serverExternalPackages: ['fluent-ffmpeg'],
-  },
   // Disable ESLint during build
   eslint: {
     ignoreDuringBuilds: true,
